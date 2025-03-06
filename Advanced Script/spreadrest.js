0c798258d49b9b866a5633ac2ca22f039ae55784
vinaneosoft <vinaneosoft@gmail.com>
2025-03-06 13:53:26 +0530

    commit

--- Advanced Script/spreadrest.js
+++ Advanced Script/spreadrest.js
@@ -8,3 +8,40 @@ console.log(add(12,45,78,90));
 console.log(add(12,45,3,4,45,6,7,7));
 console.log(add(12,45,78,90,2));
 console.log(add());
+
+let ages=[2,3,1,4,5];
+console.log(add(...ages));
+
+// copy ages array into new array
+let ages2=ages;  // no copy
+
+let ages3=[...ages, 78, 79]; // copy and new addition
+console.log(ages3);
+
+// while copying array update
+
+let emp={
+        empid:234,
+        empname:"poonam",
+        empsalary:678998998
+}
+let emp2=emp; // no copy 
+let emp3 = {...emp}; // copy
+console.log(emp3);
+
+// while copying we will update, 
+let emp4={...emp, empname:"pooja"};
+console.log(emp4);
+
+// while copying adding new key value
+let emp5={...emp, empname:"pooja", experience:20};
+console.log(emp5);
+
+
+/* let empp={
+        empid:567,
+        empname:{
+                firstname:"vina",
+                lastname:"patil"
+        }
+} */
