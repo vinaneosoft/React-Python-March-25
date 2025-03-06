9bfb0ec0423685bf5b5fb736dcfd69ded789eb89
vinaneosoft <vinaneosoft@gmail.com>
2025-03-06 13:31:17 +0530

    commit

---
+++ Advanced Script/nulltest.js
@@ -0,0 +1,8 @@
+console.log(null===undefined);
+
+let book=null;
+console.log(typeof book);
+
+let book2=undefined;
+console.log(typeof book2);
+
--- Advanced Script/spreadrest.js
+++ Advanced Script/spreadrest.js
@@ -1,11 +1,10 @@
 
-function add(...nums){
-        return nums.reduce((acc,num)=>acc+num);
-}
+const add=(...nums)=>nums.reduce((acc,num)=>acc+num,0);
+
 console.log(add(12,45));
 console.log(add(12,45,34));
 console.log(add(12));
 console.log(add(12,45,78,90));
 console.log(add(12,45,3,4,45,6,7,7));
 console.log(add(12,45,78,90,2));
-
+console.log(add());
