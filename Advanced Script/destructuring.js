0e9584888d4fa51a10d2e5ceead6a3eb434927cd
vinaneosoft <vinaneosoft@gmail.com>
2025-03-07 11:18:45 +0530

    commit

--- Advanced Script/destructuring.js
+++ Advanced Script/destructuring.js
@@ -2,11 +2,6 @@ let arr1=[78,45,89,2];
 let arr2=['hi','hello','welcome'];
 let empinfo=[101, 'harish kumar', 78000, 'married'];
 
-let empobj={
-    empid:102,
-    empname:'poona patil',
-    empsalary:90000
-}
 
 let employeeId=empinfo[0];
 let employeeName=empinfo[1];
@@ -17,4 +12,21 @@ let [eid, ename, esal]=empinfo;
 console.log(eid);
 
 
-/* object destructuring */
+let book={
+    bookId:344,
+    bookName:"Learn React Easy",
+    bookType:'Technical'
+}
+// store value of each key of an object into seperate variable
+
+let bid=book.bookId; //bid=344
+let bname=book.bookName; 
+let btype=book.bookType; 
+
+/* object destructuring : object key names == variable names*/
+
+let {bookId, bookName, bookType}= book;
+console.log(bookId); // normal variable
+//console.log(book.bookId); // key
+
+
